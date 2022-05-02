bc2498caadbd0e57e5b6ce6d6a37d5b678062f7d
Paratparshahi <khoonipratik@gmail.com>
2022-05-02 16:10:45 +0530

    navbar

---
+++ components/navbar.js
@@ -0,0 +1,7 @@
+function navbar(){
+    return`
+    <h2><a href="">NewsApp</a></h2>
+    <input type="text" id="search_input">`
+  
+}
+export default navbar;
---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// Ude Import export (MANDATORY)
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+function Searchmovies(val){
+    
+}
+import navbar from "/components/navbar.js";
+let nav=document.getElementById("navbar");
+nav.innerHTML=navbar();
+
+document.getElementById("in").addEventListener();
+document.getElementById("ch").addEventListener("click",Searchmovies("ch"));
+document.getElementById("us").addEventListener("click",Searchmovies("us"));
+document.getElementById("uk").addEventListener("click",Searchmovies("uk"));
+document.getElementById("nk").addEventListener("click",Searchmovies("nk"))
